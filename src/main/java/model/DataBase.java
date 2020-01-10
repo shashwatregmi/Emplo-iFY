310629528b8da3da042c32ddba6f3febf1415546
shashwatregmi <shashwat.regmi@icloud.com>
2020-01-10 01:30:02 -0800

    General program layout and database integration for department module.

---
+++ src/main/java/model/DataBase.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Regmi
+ */
+public class DataBase {
+    Connection dbConnect;
+    Statement dbStat;
+    
+    public DataBase() {
+        try {
+            dbConnect = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_manager", "root", "MySQL2019");
+            dbStat = dbConnect.createStatement();
+        } catch (Exception e) {
+            System.out.println("Databse Error");
+        }
+    }
+    
+    public Statement getStatement() {
+        return dbStat;
+    }
+}
---
+++ src/main/java/ui/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+import java.sql.*;
+import model.*;
+
+/**
+ *
+ * @author Regmi
+ */
+public class Main {
+    
+    public static void main(String [] args){
+        try { 
+            DataBase database = new DataBase();
+          // ResultSet myrs = database.getStatement().executeQuery("select * from department");
+            Splash splashScreen = new Splash();
+            splashScreen.setVisible(true);
+            Home homeScreen = new Home();
+            try {
+                for (int i = 0; i<=100; i++){
+                    Thread.sleep(30);
+                }
+                splashScreen.setVisible(false);
+                homeScreen.setVisible(true);
+
+            } catch (Exception e) {
+                System.out.println("Error!");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+}
